a6f103956cc85e1c41e50b7e121d0eb547a18491
yaasin12 <yassin.j@live.com>
2022-12-22 02:35:02 +0400

    Style fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 "Define a class Square."
+
+
 class Square:
     "Represent a square."
-
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
