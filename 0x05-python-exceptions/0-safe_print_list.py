821769f942957dcc0f561567d0f887f72fc82a0b
yaasin12 <yassin.j@live.com>
2022-12-22 01:50:47 +0400

    bug fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
@@ -10,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (total)
-    
