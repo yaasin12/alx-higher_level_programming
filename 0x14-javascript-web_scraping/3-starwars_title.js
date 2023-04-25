80c30bc73be6ac4cac5eb29085e5dcb2325568ec
yaasin12 <yassin.j@live.com>
2023-04-26 02:37:02 +0400

    revert

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,7 +9,7 @@ request(API_URL + episodeNum, function (err, response, body) {
     console.log(err);
   } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
-    console.log(JSON.parse(body).title);
+    console.log(responseJSON.title);
   } else {
     console.log('Error code: ' + response.statusCode);
   }
