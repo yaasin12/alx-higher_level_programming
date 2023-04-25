4370d683ab33e3535aa7edf5bbf3401bd1d31247
yaasin12 <yassin.j@live.com>
2023-04-26 02:40:26 +0400

    Fix 6

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,14 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (error, response, body) {
-  if (error) {
-    console.log(error);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
     console.log('Error code: ' + response.statusCode);
   }
